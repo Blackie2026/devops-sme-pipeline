8a71959e22ee900e86538a7b58b312afed6d388b
Blackie <blackiechukwuka@yahoo.com>
2026-03-27 00:15:22 +0100

    Performance test run 2

--- application.py
+++ application.py
@@ -1,7 +1,10 @@
+# Version 2 - Performance test run
+from flask import Flask, jsonify
+import pandas as pd
+import os
 from app.main import app
 
 application = app
 
 if __name__ == '__main__':
     application.run()
-    
